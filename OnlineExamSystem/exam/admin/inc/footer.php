a25833b292a7aa0145ba6e51d06df1ba4786704b
shuvosoftware <34794291+shuvosoftware@users.noreply.github.com>
2017-12-23 08:10:21 -0800

    Add files via upload

---
+++ OnlineExamSystem/exam/admin/inc/footer.php
@@ -0,0 +1,11 @@
+ </section>
+ <section id="footer">
+     <div class="container">
+         <div class="footer-content">
+             <p>&copy;© 2017 online exam system  <a href="#">By Shuvosarker.com</a></p>
+         </div>
+     </div>
+ </section>
+</div>
+</body>
+</html>
